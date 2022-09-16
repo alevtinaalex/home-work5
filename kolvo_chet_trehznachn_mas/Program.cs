a364448411b53c9ad18836d223a39e23d3da8ebb
Alevtina <_@email.comgit>
2022-09-16 21:44:11 +0300

    Add v massive kolvo chet chisel

---
+++ kolvo_chet_trehznachn_mas/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int size = 7;
+int[] array = new int[size];
+FillArray(array);
+System.Console.WriteLine("[{0}]", string.Join(",", array));
+Chetn(array);
+
+void FillArray(int[] array)
+{
+    for (int i=0; i< array.Length; i++)
+    {
+        array[i] = new Random().Next(100,999);  
+    }   
+}
+
+void Chetn(int[] array)
+{ 
+    int count = 0;
+    for (int i =0; i< array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count = count+1;
+        }
+    }
+    System.Console.WriteLine($"Количество четных элементов в массиве = {count}");
+}
+
